ae4ce852f994e9735775c08683dc5ec6b7b6aa38
Дмитрий <dima2005orlov11@gmail.com>
2024-11-05 12:31:14 +0300

    tz

--- database/init.sql
+++ database/init.sql
@@ -1,19 +1,3 @@
--- MySQL Workbench Forward Engineering
-
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema kursach
--- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema kursach
--- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `kursach` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
 USE `kursach` ;
 
@@ -27,10 +11,8 @@ CREATE TABLE IF NOT EXISTS `kursach`.`hotel_room` (
   `statys` VARCHAR(200) NULL DEFAULT NULL,
   `floor` INT NULL DEFAULT NULL,
   `photo` VARCHAR(45) NULL DEFAULT 'zz.jpg',
-  PRIMARY KEY (`idhotel_room`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`idhotel_room`));
+
 
 
 -- -----------------------------------------------------
@@ -42,55 +24,7 @@ CREATE TABLE IF NOT EXISTS `kursach`.`klient` (
   `Passport` VARCHAR(45) NULL DEFAULT NULL,
   PRIMARY KEY (`idklient`))
 ENGINE = InnoDB
-AUTO_INCREMENT = 31
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `kursach`.`bronirovanie`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `kursach`.`bronirovanie` (
-  `hotel_room_idhotel_room` INT NOT NULL,
-  `klient_idklient` INT NOT NULL,
-  `Date_arrival` DATE NULL DEFAULT NULL,
-  `Date_of_departure` DATE NULL DEFAULT NULL,
-  PRIMARY KEY (`hotel_room_idhotel_room`, `klient_idklient`),
-  INDEX `fk_hotel_room_has_klient_klient1_idx` (`klient_idklient` ASC) VISIBLE,
-  INDEX `fk_hotel_room_has_klient_hotel_room1_idx` (`hotel_room_idhotel_room` ASC) VISIBLE,
-  CONSTRAINT `fk_hotel_room_has_klient_hotel_room1`
-    FOREIGN KEY (`hotel_room_idhotel_room`)
-    REFERENCES `kursach`.`hotel_room` (`idhotel_room`)
-    ON DELETE CASCADE,
-  CONSTRAINT `fk_hotel_room_has_klient_klient1`
-    FOREIGN KEY (`klient_idklient`)
-    REFERENCES `kursach`.`klient` (`idklient`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `kursach`.`payment`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `kursach`.`payment` (
-  `hotel_room_idhotel_room` INT NOT NULL,
-  `klient_idklient` INT NOT NULL,
-  `cost_full` INT NULL DEFAULT NULL,
-  `receipt_number` INT NULL DEFAULT NULL,
-  PRIMARY KEY (`hotel_room_idhotel_room`, `klient_idklient`),
-  INDEX `fk_hotel_room_has_klient_klient2_idx` (`klient_idklient` ASC) VISIBLE,
-  INDEX `fk_hotel_room_has_klient_hotel_room2_idx` (`hotel_room_idhotel_room` ASC) VISIBLE,
-  CONSTRAINT `fk_hotel_room_has_klient_hotel_room2`
-    FOREIGN KEY (`hotel_room_idhotel_room`)
-    REFERENCES `kursach`.`hotel_room` (`idhotel_room`)
-    ON DELETE CASCADE,
-  CONSTRAINT `fk_hotel_room_has_klient_klient2`
-    FOREIGN KEY (`klient_idklient`)
-    REFERENCES `kursach`.`klient` (`idklient`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+AUTO_INCREMENT = 31;
 
 
 -- -----------------------------------------------------
@@ -99,10 +33,7 @@ COLLATE = utf8mb4_0900_ai_ci;
 CREATE TABLE IF NOT EXISTS `kursach`.`role` (
   `idrole` INT NOT NULL,
   `name_role` VARCHAR(45) NULL DEFAULT NULL,
-  PRIMARY KEY (`idrole`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`idrole`));
 
 
 -- -----------------------------------------------------
@@ -119,82 +50,62 @@ CREATE TABLE IF NOT EXISTS `kursach`.`user` (
   CONSTRAINT `fk_user_role`
     FOREIGN KEY (`role_idrole`)
     REFERENCES `kursach`.`role` (`idrole`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 20
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+AUTO_INCREMENT = 20;
+
+
+
+-- -----------------------------------------------------
+-- Table `kursach`.`bronirovanie`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `kursach`.`bronirovanie` (
+  `hotel_room_idhotel_room` INT NOT NULL,
+  `klient_idklient` INT NOT NULL,
+  `Date_arrival` DATE NULL DEFAULT NULL,
+  `Date_of_departure` DATE NULL DEFAULT NULL,
+  `user_iduser` INT NOT NULL,
+  PRIMARY KEY (`hotel_room_idhotel_room`, `klient_idklient`),
+  CONSTRAINT `fk_hotel_room_has_klient_hotel_room1`
+    FOREIGN KEY (`hotel_room_idhotel_room`)
+    REFERENCES `kursach`.`hotel_room` (`idhotel_room`)
+    ON DELETE CASCADE,
+  CONSTRAINT `fk_hotel_room_has_klient_klient1`
+    FOREIGN KEY (`klient_idklient`)
+    REFERENCES `kursach`.`klient` (`idklient`),
+  CONSTRAINT `fk_bronirovanie_user1`
+    FOREIGN KEY (`user_iduser`)
+    REFERENCES `kursach`.`user` (`iduser`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+
+-- -----------------------------------------------------
+-- Table `kursach`.`payment`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `kursach`.`payment` (
+  `hotel_room_idhotel_room` INT NOT NULL,
+  `klient_idklient` INT NOT NULL,
+  `cost_full` INT NULL DEFAULT NULL,
+  `receipt_number` INT NULL DEFAULT NULL,
+  PRIMARY KEY (`hotel_room_idhotel_room`, `klient_idklient`),
+  CONSTRAINT `fk_hotel_room_has_klient_hotel_room2`
+    FOREIGN KEY (`hotel_room_idhotel_room`)
+    REFERENCES `kursach`.`hotel_room` (`idhotel_room`)
+    ON DELETE CASCADE,
+  CONSTRAINT `fk_hotel_room_has_klient_klient2`
+    FOREIGN KEY (`klient_idklient`)
+    REFERENCES `kursach`.`klient` (`idklient`));
 
 USE `kursach` ;
 
--- -----------------------------------------------------
--- procedure fullcost
--- -----------------------------------------------------
 
-DELIMITER $$
-USE `kursach`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `fullcost`(IN b INT, IN c INT)
-BEGIN
-declare result int;
-    SET result = b * c;
-    select result;
-END$$
+INSERT INTO `hotel_room` VALUES (1,132,800,'Занят',1,'room1.jpg'),(2,133,1000,'Свободен',1,'room2.jpg'),(3,134,1500,'Свободен',1,'room3.jpg'),(4,231,900,'Свободен',2,'room4.jpeg'),(5,234,4000,'Свободен',2,'room5.jpg'),(6,236,3000,'Свободен',2,'room6.jpg'),(7,346,1200,'Свободен',3,'room7.jpeg'),(8,324,700,'Свободен',3,'room8.jpg'),(9,321,800,'Свободен',3,'room9.jpg');
+INSERT INTO `klient` VALUES (1,'Пряженцов михаил','2019 789534'),(2,'Поляшов Никита Михайлович','2211 089654'),(3,'Круглов Кирилл Александрович','1234 890765'),(7,'Лашков Максим','1234567898765'),(30,'Гутянская Е М','123456543');
+INSERT INTO `payment` VALUES (1,3,1600,5);
+INSERT INTO `bronirovanie` VALUES (1,3,'2023-11-22','2023-11-24');
+INSERT INTO `role` VALUES (1,'Administrator'),(2,'Person');
+INSERT INTO `user` VALUES (1,'Орлов Дмитрий Андреевич','adm','123',1),(2,'Сокуров Александ Владимирович','per','12',2),(7,'grew','qwerty','21',2),(8,'Круглов Кирилл Александрович','st1','567',2),(9,'Кургузов','qwa','143',2),(10,'1','2','3',2),(11,'Травкина А. В.','trav','11052006',2),(12,'bhgf','bgvf','g',2),(18,'енг','per1','12',2),(19,'42','per12','tr',2);
 
-DELIMITER ;
-USE `kursach`;
-
-DELIMITER $$
-USE `kursach`$$
-CREATE
-DEFINER=`root`@`localhost`
-TRIGGER `kursach`.`hotel_room_BEFORE_UPDATE`
-BEFORE UPDATE ON `kursach`.`hotel_room`
-FOR EACH ROW
-BEGIN
- IF NEW.statys != 'Свободен' AND NEW.statys != 'Занят' THEN
-       SET NEW.statys = OLD.statys;
-     END IF;
-END$$
-
-USE `kursach`$$
-CREATE
-DEFINER=`root`@`localhost`
-TRIGGER `kursach`.`bronirovanie_BEFORE_DELETE`
-BEFORE DELETE ON `kursach`.`bronirovanie`
-FOR EACH ROW
-BEGIN
-UPDATE hotel_room
-    SET statys = 'Свободен'
-    WHERE idhotel_room = OLD.hotel_room_idhotel_room;
-END$$
-
-USE `kursach`$$
-CREATE
-DEFINER=`root`@`localhost`
-TRIGGER `kursach`.`bronirovanie_BEFORE_INSERT`
-BEFORE INSERT ON `kursach`.`bronirovanie`
-FOR EACH ROW
-BEGIN
- IF NEW.Date_arrival>new.Date_of_departure  THEN
-        SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'Дата приезда не может быть больше даты уезда"';
-    END IF;
-END$$
-
-USE `kursach`$$
-CREATE
-DEFINER=`root`@`localhost`
-TRIGGER `kursach`.`payment_AFTER_INSERT`
-AFTER INSERT ON `kursach`.`payment`
-FOR EACH ROW
-BEGIN
-UPDATE hotel_room
-SET statys = 'Занят'
-WHERE idhotel_room = NEW.hotel_room_idhotel_room;
-END$$
 
 
 DELIMITER ;
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
